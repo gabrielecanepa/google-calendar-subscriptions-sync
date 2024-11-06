699b59b9e0f03842930c9bb65f80399466689f81
Gabriele Canepa <git@gabrielecanepa.com>
2024-11-06 17:56:20 +0100

    feat(football): remove italy calendar

--- src/subscriptions/football/data.ts
+++ src/subscriptions/football/data.ts
@@ -1,6 +1,6 @@
 export const MATCH_BASE_URL = 'https://forzafootball.com/match'
 export const TEAM_BASE_URL = 'https://api.forzafootball.net/v1/teams'
-export const TEAM_IDS = [8292, 9363]
+export const TEAM_IDS = [9363]
 
 export const STADIUM_REGEX = /[Ss]tadi(o|on|um)/
 export const TIMEZONE = 'Europe/Rome'
